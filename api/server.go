f2372fae5750f4e698277e438dd97bdf148e51e3
khavq <khait90@gmail.com>
2019-12-19 16:33:45 +0700

    deploy heroku

--- api/server.go
+++ api/server.go
@@ -20,13 +20,13 @@ func init() {
 
 func Run() {
 
-	var err error
-	err = godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error getting env, %v", err)
-	} else {
-		fmt.Println("We are getting values")
-	}
+	//var err error
+	//err = godotenv.Load()
+	//if err != nil {
+	//log.Fatalf("Error getting env, %v", err)
+	//} else {
+	//fmt.Println("We are getting values")
+	//}
 
 	server.Initialize(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
 
